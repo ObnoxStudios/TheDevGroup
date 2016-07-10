801e66d3137449a984306e88c2810a5a06a96384
Anthony Taha <obnoxstudios@gmail.com>
2016-07-10 14:28:26 +0000

    Finished Basic Fonctionality And getting ready for heroku Deployment as as preAlphaRelease

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'devise'
+gem 'devise', '~> 3.5', '>= 3.5.2'
 gem 'bootstrap-sass' ,'~> 3.3.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
   root'pages#index'
   get '/index' => 'pages#index'
   get '/home' => 'pages#home'
-  get '/profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
+  get '/help' => 'pages#help'
+  get '/users' => 'pages#User'
 
 end
